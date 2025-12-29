eb611da24c7a6753eb44138167301b89247fb132
LohithS22 <lohiths2212@gmail.com>
2025-12-30 00:05:46 +0530

    Refactored frontend

---
+++ frontend/app/dashboard/page.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div className="min-h-screen bg-linear-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+            <h1 className="text-white text-4xl font-bold">map here</h1>
+        </div>
+    );
+}
