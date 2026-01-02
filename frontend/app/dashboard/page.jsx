f3e0c9a757ae28d8717d4b732b20fdea272933d5
LohithS22 <lohiths2212@gmail.com>
2026-01-02 21:28:39 +0530

    User Location fetching feature added

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default function Dashboard() {
+
+    const [ location, setLocation ] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLocation([
+                    position.coords.latitude,
+                    position.coords.longitude
+                ]);
+            },
+            () => {
+                alert("Location access is required to cotinue.");
+            }
+        );
+    },[]);
+
     return (
-        <div className="min-h-screen bg-linear-to-br from-blue-500 to-purple-600 flex items-center justify-center">
-            <h1 className="text-white text-4xl font-bold">map here</h1>
+        <div className="min-h-screen ">
+            {location ? (
+                < MapContainer center={location} zoom={14} className= "h-screen w-full" >
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={location}>
+                        <Popup>Your current location</Popup>
+                    </Marker>
+                </MapContainer>
+            ) : (
+                <div className="h-screen flex items-center justify-center text-xl">
+                    Fetching your location...    
+                </div>
+            )}
         </div>
     );
 }
